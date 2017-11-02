345c3d6793422bfd8ee9be928faba2fd8131f38c
shrooqag <32885243+shrooqag@users.noreply.github.com>
2017-11-02 00:32:14 -0700

    Add files via upload

---
+++ tst/SA/test.cpp
@@ -0,0 +1,19 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+#include <stdlib.h>
+
+//#include "../../inc/SA/customer.h"
+//#include "../../inc/SA/population.h"
+//#include "../../src/SA/PopulationConfiguration.cpp"
+#include "../../src/SA/customer.cpp"
+#include "../../src/SA/population.cpp"
+
+TEST_CASE( "Population not negetive", "[population]" ) {
+	SAPopulation p;
+    REQUIRE( p.createPopulation() >= 0 );
+}
+
+TEST_CASE( "Population not more than the maximum of population size", "[population]" ) {
+	SAPopulation p;
+    REQUIRE( p.createPopulation() <= MAX_SIZE );
+}
