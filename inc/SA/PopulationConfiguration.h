e4d7001f7b4de60a8efe57745177af0c0d4a3271
shrooqag <32885243+shrooqag@users.noreply.github.com>
2017-11-01 23:53:55 -0700

    Delete PopulationConfiguration.h

--- inc/SA/PopulationConfiguration.h
+++
@@ -1,5 +0,0 @@
-class SAPopulationConfiguration{
-   const static int MAX_SIZE = 2000; 
-   const DOUBLE DAY_OF_WEEK[] = {.7,.2,.2,.2,.2,.2,0.7};  // 0=sun, 1=mon, 2=tue, 3=wed, 4=thu, 5=fri, 6=sat
-   const DOUBLE WEATHER[] = {.1,.3,.2,.7,.5};  // 0=snow, 1=cold, 2=rainy, 3=moderate, 4=sunny
-};
