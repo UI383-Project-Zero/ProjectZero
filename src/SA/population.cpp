070934ff507f1f960bb6aa5eeec4fbac1f9da05b
shrooqag <32885243+shrooqag@users.noreply.github.com>
2017-12-01 12:13:26 -0800

    Add files via upload

--- src/SA/population.cpp
+++ src/SA/population.cpp
@@ -2,63 +2,124 @@
 
 #include "../../inc/SA/population.h"
 #include "PopulationConfiguration.cpp"
-using namespace std;
 
-
-
-SAPopulation::SAPopulation(){
-  cout << "SAPopulation() Constructor."<< endl;
+/**
+* Constructor population.
+*
+* @param 
+* @return 
+*/
+SAPopulation::SAPopulation() {
+  //std::cout << "SAPopulation() Constructor.\n";
 }
 
-SAPopulation::	~SAPopulation(){
-  cout << "~SAPopulation() Destructor."<< endl;
+/**create population
+* this is for singleton pattern 
+*
+* @return 
+*/
+SAPopulation* SAPopulation::getMUniqueInstance() {
+	if (mUniqueInstance == NULL) {
+		mUniqueInstance = new SAPopulation();
+		std::cout << "population instant created\n";
+	}
+	return mUniqueInstance;
 }
+
+/**
+* Set population size.
+*
+* @param i to be population size
+* @return void
+*/
+
 void SAPopulation::setMPopSize(int i) {
-	cout << "SACustomer::setMPopSize(int)" << endl;
+	//std::cout << "SACustomer::setMPopSize(int)\n";
 	mPopSize = i;
 }
 
+/**
+* Get population size.
+*
+* @return population size
+*/
 int SAPopulation::getMPopSize() {
-	cout << "SACustomer::getMPopSize()" << endl;
+	//std::cout << "SACustomer::getMPopSize()\n";
 	return 	mPopSize;
 }
-void SAPopulation::setMCustomers(vector<SACustomer> x){
-	cout << "SAPopulation::setMCustomers(vector<SACustomer>)" << endl;
+
+/**
+* Set customers.
+*
+* @param x to be customers
+* @return void
+*/
+void SAPopulation::setMCustomers(std::vector<SACustomer*> x) {
+	//std::cout << "SAPopulation::setMCustomers(vector<SACustomer>)\n";
 	mCustomers=x;
 }
-vector<SACustomer> SAPopulation::getMCustomers(){
-	cout << "SAPopulation::getMCustomers()" << endl;
+
+/**
+* Get customers.
+* 
+* @return customers.
+*/
+std::vector<SACustomer*> SAPopulation::getMCustomers() {
+	//cout << "SAPopulation::getMCustomers()";
 	return mCustomers;
 }
-int SAPopulation::createPopulation() {
-  cout << "SAPopulation::createPopulation()"<< endl ;
-  int initialSize = 0;
-  initialSize = SAPopulationConfig::generateSize();
-  return initialSize;
-}
 
-void SAPopulation::addMCustomers(SACustomer cust){
-  cout << "SAPopulation::addMCustomers(SACustomer)"<< endl ;
+/**
+* Create population.
+*
+* @param 
+* @return initial population
+*/
+//int SAPopulation::createPopulation() {
+  //std::cout << "SAPopulation::createPopulation()\n";
+  //int initialSize = 0;
+  //initialSize = SAPopulationConfig::generateSize();
+  //return initialSize;
+//}
+
+/**
+* Adds new customer to the population.
+*
+* @param cust customer to be added in population
+* @return void
+*/
+void SAPopulation::addMCustomers(SACustomer* cust) {
+  //std::cout << "SAPopulation::addMCustomers(SACustomer)\n";
   mCustomers.push_back (cust);
 }
 
-
- void SAPopulation::removeMCustomers(SACustomer cust){
-  cout << "SAPopulation::removeMCustomers(SACustomer)"<< endl;
-  //mCustomers.erase (mCustomers.begin() + 5);  // think logic to remove from vector
-  /*int index = 0;
-  for (std::vector<SACustomer>::iterator it = mCustomers.begin() ; it != mCustomers.end(); ++it)
-    SACustomer c = *it;
-    if(cust.getMCustID() == c.getMCustID()) {
-    	index = 
-	}*/
+/**
+* Delete customers from the population.
+*
+* @param cust customer to be removed from population
+* @return bool true if found item to remove, false if not found
+*/
+bool SAPopulation::removeMCustomers(int custId) {
+    //std::cout << "SAPopulation::removeMCustomers(SACustomer)\n";
+    bool isFound = false;
+    std::vector <SACustomer*>::iterator iter;
+    for (iter = mCustomers.begin(); iter != mCustomers.end(); ++iter) {
+        if ((*iter)->getMCustID() == custId) {
+            iter = mCustomers.erase(iter); // After erasing, iter is now pointing the next location.
+            --iter; // Go to the previous location because of ++iter in the end of for loop.
+            isFound = true;
+        }
+    }
+    
+    return isFound;
 }
 
-void SAPopulation::updateMCustomers(int cust){
-
-  cout << "SAPopulation::updateMCustomers(int)"<< endl;
-
-}
-
-
-
+/**
+* Update customers.
+*
+* @param cust customer 
+* @return void
+*/
+//void SAPopulation::updateMCustomers(int cust) {
+  //std::cout << "SAPopulation::updateMCustomers(int)\n";
+//}
