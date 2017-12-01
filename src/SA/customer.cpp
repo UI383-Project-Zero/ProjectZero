2b445b589e721d294618c8035066e4a168218525
shrooqag <32885243+shrooqag@users.noreply.github.com>
2017-12-01 12:12:21 -0800

    Add files via upload

--- src/SA/customer.cpp
+++ src/SA/customer.cpp
@@ -2,108 +2,298 @@
 
 #include "../../inc/SA/customer.h"
 
-using namespace std;
 
-SACustomer::SACustomer(){
-  cout << "SACustomer() Constructor" << endl;
-}
-
-SACustomer::SACustomer(int id){
-  cout << "SACustomer(int) Constructor" << endl;
-  mCustID = id;
-}
-
-SACustomer::~SACustomer(){
-  cout << "~SACustomer() Destructor" << endl;
-}
+/**
+* Set customer ID.
+*
+* @param c to be customer ID
+* @return void
+*/
 void SACustomer::setMCustID(int c) {
-	cout << "SACustomer::setMCustID(int)" << endl;
+	//std::cout << "SACustomer::setMCustID(int)\n";
 	mCustID = c;
 }
 
+/**
+* Get customer ID.
+*
+* @param 
+* @return customer ID.
+*/
 int SACustomer::getMCustID() {
-	cout << "SACustomer::getMCustID()" << endl;
+	//std::cout << "SACustomer::getMCustID()\n";
 	return 	mCustID;
 }
 
+/**
+* Set satisfaction.
+*
+* @param s to be satisfaction.
+* @return void
+*/
 void SACustomer::setMSatisfaction(int s) {
-	cout << "SACustomer::setMSatisfaction(int)" << endl;
+	//std::cout << "SACustomer::setMSatisfaction(int)\n";
 	mSatisfaction = s;
 }
 
+/**
+* Get satisfaction.
+*
+* @return satisfaction.
+*/
 int SACustomer::getMSatisfaction() {
-	cout << "SACustomer::getMSatisfaction()" << endl;
+	//std::cout << "SACustomer::getMSatisfaction()\n";
 	return mSatisfaction;
 }
+
+/**
+* Set money.
+*
+* @param m to be money.
+* @return void
+*/
 void SACustomer::setMMoney(double m) {
-	cout << "SACustomer::setMMoney(double)" << endl;
+	//std::cout << "SACustomer::setMMoney(double)\n";
 	mMoney= m;
 }
 
+/**
+* Get money.
+* 
+* @return money.
+*/
 double SACustomer::getMMoney() {
-	cout << "SACustomer::getMMoney()" << endl;
+	//std::cout << "SACustomer::getMMoney()\n";
 	return mMoney;
 }
+
+/**
+* Set tickets.
+*
+* @param t to be tickets.
+* @return void
+*/
 void SACustomer::setMTickets(int t) {
-	cout << "SACustomer::setMTickets(int)" << endl;
+	//std::cout << "SACustomer::setMTickets(int)\n";
 	mTickets = t;
 }
 
+/**
+* Get tickets.
+* 
+* @return tickets.
+*/
 int SACustomer::getMTickets() {
-	cout << "SACustomer::getMTickets()" << endl;
+	//std::cout << "SACustomer::getMTickets()\n";
 	return mTickets;
 }
+
+/**
+* Set hunger.
+*
+* @param h to be hunger.
+* @return void
+*/
 void SACustomer::setMHunger(int h) {
-	cout << "SACustomer::setMHunger(int)" << endl;
+	//std::cout << "SACustomer::setMHunger(int)\n";
     mHunger = h;
 }
 
+/**
+* Get hunger.
+*
+* @return hunger.
+*/
 int SACustomer::getMHunger() {
-	cout << "SACustomer::getMHunger()" << endl;
+	//std::cout << "SACustomer::getMHunger()\n";
 	return mHunger;
 }
+
+/**
+* Set stamina.
+*
+* @param a to be stamina.
+* @return void
+*/
 void SACustomer::setMStamina(int a) {
-	cout << "SACustomer::setMStamina(int)" << endl;
+	//std::cout << "SACustomer::setMStamina(int)\n";
     mStamina = a;
 }
 
+/**
+* Get stamina.
+* 
+* @return stamina.
+*/
 int SACustomer::getMStamina() {
-	cout << "SACustomer::getMStamina()" << endl;
+	//std::cout << "SACustomer::getMStamina()\n";
 	return  mStamina;
 }
+
+/**
+* Set Nausea
+*
+* @param n to be Nausea.
+* @return void
+*/
 void SACustomer::setMNausea(int n) {
-	cout << "SACustomer::setMNausea(int)" << endl;
+	//std::cout << "SACustomer::setMNausea(int)\n";
     mNausea = n;
 }
 
+/**
+* Get Nausea.
+*
+* @return Nausea.
+*/
 int SACustomer::getMNausea() {
-	cout << "SACustomer::getMNausea()" << endl;
+	//std::cout << "SACustomer::getMNausea()\n";
 	return  mNausea;
 }
+
+/**
+* Set Patience.
+*
+* @param p to be Patience.
+* @return void
+*/
 void SACustomer::setMPatience(int p) {
-	cout << "SACustomer::setMPatience(int)" << endl;
+	//std::cout << "SACustomer::setMPatience(int)\n";
     mPatience = p;
 }
 
+/**
+* Get Patience.
+*
+* @return Patience.
+*/
 int SACustomer::getMPatience() {
-	cout << "SACustomer::getMPatience()" << endl;
+	//std::cout << "SACustomer::getMPatience()\n";
 	return  mPatience;
 }
+
+/**
+* Set busy
+*
+* @param b 
+* @return void
+*/
 void SACustomer::setMBusy(bool b) {
-	cout << "SACustomer::setMBusy(bool)" << endl;
+	//std::cout << "SACustomer::setMBusy(bool)\n";
     mBusy = b;
 }
 
+/**
+* Get busy 
+* 
+* @return bool 
+*/
 bool SACustomer::getMBusy() {
-	cout << "SACustomer::getMBusy()" << endl;
+	//std::cout << "SACustomer::getMBusy()\n";
 	return  mBusy;
 }
+
+/**
+* Set queue
+*
+* @param q 
+* @return void
+*/
 void SACustomer::setMQueue(bool q) {
-	cout << "SACustomer::setMQueue(bool)" << endl;
+	//std::cout << "SACustomer::setMQueue(bool)\n";
     mQueue = q;
 }
 
+/**
+* Get queue 
+*
+* @return bool 
+*/
 bool SACustomer::getMQueue() {
-	cout << "SACustomer::getMQueue()" << endl;
+	//std::cout << "SACustomer::getMQueue()\n";
 	return   mQueue;
 }
+
+
+class SACustomerAdult: public SACustomer {
+public:
+    void init() {
+        //std::cout << "SACustomerAdult::init()\n";
+    }
+    
+    void setMTicketPrice(double d) {
+        //std::cout << "SACustomerAdult::setMTicketPrice(double)\n";
+        mTicketPrice = d;
+    }
+    
+    double getMTicketPrice() {
+        //std::cout << "SACustomerAdult::getMTicketPrice()\n";
+        return mTicketPrice;
+    }
+};
+
+class SACustomerStudent: public SACustomer {
+public:
+    void init() {
+        //std::cout << "SACustomerStudent::init()\n";
+    }
+    
+    void setMTicketPrice(double d) {
+        //std::cout << "SACustomerStudent::setMTicketPrice(double)\n";
+        mTicketPrice = 0.7 * d;
+    }
+    
+    double getMTicketPrice() {
+        //std::cout << "SACustomerStudent::getMTicketPrice()\n";
+        return mTicketPrice;
+    }
+};
+
+class SACustomerChild: public SACustomer {
+public:
+    void init() {
+        //std::cout << "SACustomerChild::init()\n";
+    }
+    
+    void setMTicketPrice(double d) {
+        //std::cout << "SACustomerChild::setMTicketPrice(double)\n";
+        mTicketPrice = 0.0 * d;
+    }
+    
+    double getMTicketPrice() {
+        //std::cout << "SACustomerChild::getMTicketPrice()\n";
+        return mTicketPrice;
+    }
+};
+
+
+// Factory class to generate different customers
+class SACustomerFactory {
+private:
+    SACustomer *mSACustomer;
+public:
+    SACustomerFactory(std::string type)  {
+        // SACustomerFactory explicitly creates classes according to type
+        if ("a" == type) {
+            mSACustomer = new SACustomerAdult();
+        } else if ("s" == type) {
+            mSACustomer = new SACustomerStudent();
+        } else if ("c" == type) {
+            mSACustomer = new SACustomerChild();
+        } else {
+            mSACustomer = NULL;
+        }
+    }
+    
+    ~SACustomerFactory()   {
+        if (mSACustomer)
+        {
+            delete[] mSACustomer;
+            mSACustomer = NULL;
+        }
+    }
+    
+    SACustomer* getSACustomer() {
+        return mSACustomer;
+    }
+};
