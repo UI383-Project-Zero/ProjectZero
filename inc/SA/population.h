6db3efea4353475dbb8a59a4baf740d9fce43ddf
shrooqag <32885243+shrooqag@users.noreply.github.com>
2017-12-01 12:10:58 -0800

    Add files via upload

--- inc/SA/population.h
+++ inc/SA/population.h
@@ -5,21 +5,25 @@
 #include "customer.h"
 
 class SAPopulation {
-
 private:
+	SAPopulation();  // for making singleton
+	static SAPopulation *mUniqueInstance;  // for making singleton
+    static int nextId;  // to automatically assign id 
 	int mPopSize;
-    vector<SACustomer> mCustomers;
-
+    std::vector<SACustomer*> mCustomers;
 public:
-	SAPopulation();
-	~SAPopulation();
+    static SAPopulation* getMUniqueInstance();
 	void setMPopSize(int);
 	int getMPopSize();
-	void setMCustomers(vector<SACustomer>);
-	vector<SACustomer> getMCustomers();
-    int createPopulation();
-    void addMCustomers(SACustomer);
-    void removeMCustomers(SACustomer);
-    void updateMCustomers(int);
+	void setMCustomers(std::vector<SACustomer*>);
+	std::vector<SACustomer*> getMCustomers();
+    void addMCustomers(SACustomer*);
+    bool removeMCustomers(int);
+    
+    static int getNextId() {return ++nextId;}
 };
-#endif  // SA_POPULATION_H_
+// initialize static singleton pointer
+SAPopulation *SAPopulation::mUniqueInstance = NULL;  // initialize unique instance to NULL at begining
+// initialize customer id counter
+int SAPopulation::nextId = 0;
+#endif  // SA_POPULATION_H__
